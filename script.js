3a46d0ac585c6c1a3c93d826c29061ff0ff6d965
NasNanaDev <nas.nana.devloper@gmail.com>
2024-10-16 17:10:58 +0300

    Add basic JS

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-let numbers = document.querySelectorAll('.number');
-let number  
-
-
-
-
 let result = document.querySelector('.result');
 
 
-function plusNumbers() {
-    result.innerHTML = number + number;
-    return result;
-}
-function minusNumbers() {
-    result.innerHTML = number - number;
-    return result;
-}
-function multiplyNumbers() {
-    result.innerHTML = number * number;
-    return result;
-}
-function divideNumbers() {
-    result.innerHTML = number / number;
-    return result;
-}
 
 
-minusNumbers()
+function toDisplay(input){
+    result.value += input;
+}
 
-console.log(numbers)
+function clearNum(){
+    result.value = '';
+}
+
+function equal(){
+    try {
+        result.value = eval(result.value);
+    } catch (error) {
+        result.value = 'Error'
+    }
+}
+''
